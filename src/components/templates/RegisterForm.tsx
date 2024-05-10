0679d183976ccc0d90165238a17562a7583af66e
kodai <kodai030513@gmail.com>
2024-05-10 11:20:44 +0900

    新規登録画面の実装

--- src/components/templates/RegisterForm.tsx
+++ src/components/templates/RegisterForm.tsx
@@ -1,25 +1,23 @@
-import Button from "../parts/Button";
+import Button from "../parts/Button"
 import InputText from "../parts/InputText"
-import LinkText from "../parts/LinkText";
+import LinkText from "../parts/LinkText"
 
-const LoginForm: React.FC = () => {
+const RegisterForm: React.FC = () => {
     return (
         <div className="w-full max-w-lg">
             <div className="flex flex-wrap -mx-3 mt-6 mb-6">
                 <div className="w-full px-3 mt-6 mb-6 md:mb-0">
-                    <p className="text-2xl text-center">ログイン</p>
-                </div>
-                <div className="w-full px-3 mt-6 md:mb-0">
-                    <div className="text-right">
-                        <LinkText
-                            link="/register"
-                            name="会員登録はこちら"
-                        />
-                    </div>
+                    <p className="text-2xl text-center">会員登録</p>
                 </div>
                 <div className="w-full px-3 mb-6 md:mb-0">
                     <InputText
                         name="ユーザー名"
+                        type="text"
+                    />
+                </div>
+                <div className="w-full px-3 mt-6 mb-6 md:mb-0">
+                    <InputText
+                        name="メールアドレス"
                         type="text"
                     />
                 </div>
@@ -31,21 +29,21 @@ const LoginForm: React.FC = () => {
                 </div>
                 <div className="w-full px-3 mt-6 mb-6 md:mb-0">
                     <Button
-                        name="ログイン"
+                        name="登録"
                         isWeightFull={true}
                     />
                 </div>
                 <div className="w-full px-3 mt-6 md:mb-0">
                     <div className="text-right">
                         <LinkText
-                            link="/password/reset"
-                            name="パスワードを忘れた方はこちら"
+                            link="/login"
+                            name="アカウントをお持ちのからはこちら"
                         />
                     </div>
                 </div>
             </div>
         </div>
     )
-};
+}
 
-export default LoginForm;
+export default RegisterForm;
---
+++ src/components/views/RegisterFormView.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from "../templates/RegisterForm";
+
+const RegisterFormView: React.FC = () => {
+    return (
+        <RegisterForm />
+    );
+};
+
+export default RegisterFormView;
