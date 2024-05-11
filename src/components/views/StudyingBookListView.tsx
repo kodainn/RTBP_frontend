2174feb3fdef1cbf97adf8fd3d5cfc86ac3e8601
kodai <kodai030513@gmail.com>
2024-05-12 00:55:15 +0900

    カードのレイアウトの修正

--- src/components/views/StudyingBookListView.tsx
+++ src/components/views/StudyingBookListView.tsx
@@ -1,26 +1,27 @@
 import StudyingBookCard from "../templates/StudyingBookCard";
 
 const StudyingBookListView: React.FC = () => {
+
     return (
         <div className="mt-24">
-                <div className="grid grid-cols-3">
-                    <div className="mr-12 ml-12 mt-6 mb-6">
-                        <StudyingBookCard />
-                    </div>
-                    <div className="mr-12 ml-12 mt-6 mb-6">
-                        <StudyingBookCard />
-                    </div>
-                    <div className="mr-12 ml-12 mt-6 mb-6">
-                        <StudyingBookCard />
-                    </div>
-                    <div className="mr-12 ml-12 mt-6 mb-6">
-                        <StudyingBookCard />
-                    </div>
-                    <div className="mr-12 ml-12 mt-6 mb-6">
-                        <StudyingBookCard />
-                    </div>
+            <div className="flex flex-wrap">
+                <div className="w-full sm:w-1/2 xl:w-1/3 p-4">
+                    <StudyingBookCard />
+                </div>
+                <div className="w-full sm:w-1/2 xl:w-1/3 p-4">
+                    <StudyingBookCard />
+                </div>
+                <div className="w-full sm:w-1/2 xl:w-1/3 p-4">
+                    <StudyingBookCard />
+                </div>
+                <div className="w-full sm:w-1/2 xl:w-1/3 p-4">
+                    <StudyingBookCard />
+                </div>
+                <div className="w-full sm:w-1/2 xl:w-1/3 p-4">
+                    <StudyingBookCard />
                 </div>
             </div>
+        </div>
     );
 };
 
