515b1cb025f84c3e9b04fa568ac1dd2590ffd9c3
kodai <kodai030513@gmail.com>
2024-05-27 12:40:22 +0900

    学習書籍登録機能を実装

--- src/components/templates/BookDetailCard.tsx
+++ src/components/templates/BookDetailCard.tsx
@@ -1,19 +1,26 @@
-import phpBookImage from "../../assets/php.jpg";
+import defaultBookImage from "../../assets/default_book.png";
 
-const BookDetailCard: React.FC = () => {
+type Props = {
+    shelveName: string,
+    title:       string,
+    remark:      string,
+    imgUrl:      string
+}
+
+const BookDetailCard: React.FC<Props> = ({ shelveName, title, remark, imgUrl }) => {
     return (
         <div className="bg-white max-w-xl rounded overflow-hidden shadow-lg">
             <div className="flex justify-center">
-                <img className="w-[70%]" src={phpBookImage} alt="本の画像" />
+                <img className="w-[70%]" src={imgUrl ? imgUrl : defaultBookImage} alt="本の画像" />
             </div>
             <div className="px-6 py-4">
-                <div className="font-bold text-2xl mb-2">PHPの絵本</div>
+                <div className="font-bold text-2xl mb-2">{title}</div>
                 <p className="text-gray-700 text-base">
-                    書学者向けPHPの入門書
+                    {remark}
                 </p>
             </div>
             <div className="px-6 pt-4 pb-2">
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">webアプリ開発</span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{shelveName}</span>
             </div>
         </div>
     );
