0e3727f743af64c3582ec17c31f56139d165ed71
kodai <kodai030513@gmail.com>
2024-05-29 07:43:58 +0900

    ログイン機能を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import DashboardPage from "./pages/DashboardPage";
@@ -18,24 +19,26 @@ import ShelveEditPage from "./pages/ShelveEditPage";
 const App: React.FC = () => {
   return (
     <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path={"/login"} element={<LoginPage />} />
-          <Route path={"/register"} element={<RegisterPage />} />
-          <Route path={"/dashboard"} element={<DashboardPage />} />
-          <Route path={"/shelves"} element={<ShelveListPage />} />
-          <Route path={"/shelves/:id/books"} element={<BookListPage />} />
-          <Route path={"/shelves/create"} element={<ShelveCreatePage />} />
-          <Route path={"/shelves/:id/edit"} element={<ShelveEditPage />} />
-          <Route path={"/shelves/:id/books/create"} element={<BookCreatePage />} />
-          <Route path={"/shelves/books/:id/edit"} element={<BookEditPage />} />
-          <Route path={"/books/:id/studies/create"} element={<StudyBookCreatePage />} />
-          <Route path={"/studying-books"} element={<StudyingBookListPage />} />
-          <Route path={"/studying-books/:id/record"} element={<StudyingBookRecordPage />} />
-          <Route path={"/studied-history-books"} element={<StudiedBookHistoryListPage />} />
-          <Route path={"/studied-history-books/:book_id/show"} element={<StudiedBookHistoryShowPage />} />
-        </Routes>
-      </BrowserRouter>
+      <CookiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path={"/login"} element={<LoginPage />} />
+            <Route path={"/register"} element={<RegisterPage />} />
+            <Route path={"/dashboard"} element={<DashboardPage />} />
+            <Route path={"/shelves"} element={<ShelveListPage />} />
+            <Route path={"/shelves/:id/books"} element={<BookListPage />} />
+            <Route path={"/shelves/create"} element={<ShelveCreatePage />} />
+            <Route path={"/shelves/:id/edit"} element={<ShelveEditPage />} />
+            <Route path={"/shelves/:id/books/create"} element={<BookCreatePage />} />
+            <Route path={"/shelves/books/:id/edit"} element={<BookEditPage />} />
+            <Route path={"/books/:id/studies/create"} element={<StudyBookCreatePage />} />
+            <Route path={"/studying-books"} element={<StudyingBookListPage />} />
+            <Route path={"/studying-books/:id/record"} element={<StudyingBookRecordPage />} />
+            <Route path={"/studied-history-books"} element={<StudiedBookHistoryListPage />} />
+            <Route path={"/studied-history-books/:book_id/show"} element={<StudiedBookHistoryShowPage />} />
+          </Routes>
+        </BrowserRouter>
+      </CookiesProvider>
     </React.StrictMode>
   )
 }
--- src/components/templates/LoginForm.tsx
+++ src/components/templates/LoginForm.tsx
@@ -1,17 +1,84 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Button from "../parts/Button";
 import InputField from "../parts/InputField"
 import Label from "../parts/Label";
 import LinkText from "../parts/LinkText";
 import SuccessAlertMessage from "../parts/SuccessAlertMessage";
+import FaildAlertMessage from "../parts/FaildAlertMessage";
+import { ChangeEvent, useState } from "react";
+import { isRequired } from "../../utils/validate";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import ValidateText from "../parts/ValidateText";
+import { useCookies } from "react-cookie";
 
 const LoginForm: React.FC = () => {
 
+    const navigate = useNavigate();
+
     const loginStatus = useLocation().state;
+
+    const [ _, setCookies ] = useCookies();
+
+    const [ usernameInput, setUsernameInput ] = useState<string>("");
+    const changeUsernameInput = (event: ChangeEvent<HTMLInputElement>): void => {
+        setUsernameInput(event.target.value);
+    }
+    const [ passwordInput, setPasswordInput ] = useState<string>("");
+    const changePasswordInput = (event: ChangeEvent<HTMLInputElement>): void => {
+        setPasswordInput(event.target.value);
+    }
+
+    const [ usernameValidateMessage, setUsernameValidateMessage ] = useState<string>("");
+    const [ passwordValidateMessage, setPasswordValidateMessage ] = useState<string>("");
+
+    const formValidate = (): boolean => {
+        setUsernameValidateMessage("");
+        setPasswordValidateMessage("");
+
+        let isValidate = false;
+
+        if(!isRequired(usernameInput)) {
+            setUsernameValidateMessage("必須入力です。");
+            isValidate = true;
+        }
+
+        if(!isRequired(passwordInput)) {
+            setPasswordValidateMessage("必須項目です。");
+            isValidate = true;
+        }
+
+        return isValidate;
+    }
+
+    const createReqBody = (name: string, password: string): any => {
+        return {
+            name:     name,
+            password: password
+        }
+    }
+
+    const sendForm = (name: string, password: string): void => {
+        const isValidate = formValidate();
+        if(isValidate) return;
+
+        const reqBody = createReqBody(name, password);
+
+        axios.post(import.meta.env.VITE_API_URL + "/token", reqBody)
+        .then((res: AxiosResponse<any>) => {
+            if(res.status === 201) {
+                setCookies("access_token", res.data.access_token);
+                navigate("/dashboard", {state: {message: "ログインしました。", type: "success"}});
+            }
+        })
+        .catch((error: AxiosError<any>) => {
+            navigate("/login", {state: {message: "ログインに失敗しました。", type: "faild"}});
+        });
+    }
 
     return (
         <div className="w-full max-w-lg">
             {loginStatus !== null && loginStatus.type === "success" && <SuccessAlertMessage message={loginStatus.message} />}
+            {loginStatus !== null && loginStatus.type === "faild" && <FaildAlertMessage message={loginStatus.message} />}
             <div className="flex flex-wrap -mx-3 mt-6 mb-6">
                 <div className="w-full px-3 mt-6 mb-6 md:mb-0">
                     <p className="text-2xl text-center">ログイン</p>
@@ -26,16 +93,24 @@ const LoginForm: React.FC = () => {
                 </div>
                 <div className="w-full px-3 mb-6 md:mb-0">
                     <Label name="ユーザー名" size="base" />
-                    <InputField/>
+                    <InputField
+                        onChange={changeUsernameInput}
+                        value={usernameInput}
+                    />
+                    <ValidateText message={usernameValidateMessage} />
                 </div>
                 <div className="w-full px-3 mt-6 mb-6 md:mb-0">
                     <Label name="パスワード" size="base" />
                     <InputField
+                        onChange={changePasswordInput}
+                        value={passwordInput}
                         type="password"
                     />
+                    <ValidateText message={passwordValidateMessage} />
                 </div>
                 <div className="w-full px-3 mt-6 mb-6 md:mb-0">
                     <Button
+                        onClick={() => sendForm(usernameInput, passwordInput)}
                         name="ログイン"
                         isWeightFull={true}
                     />
