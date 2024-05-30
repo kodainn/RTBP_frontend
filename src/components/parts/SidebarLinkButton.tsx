cf9e3e20fd51662fb8f5278e99d69fe05559a401
kodai <kodai030513@gmail.com>
2024-05-30 22:11:19 +0900

    ログアウト機能実装

--- src/components/parts/SidebarLinkButton.tsx
+++ src/components/parts/SidebarLinkButton.tsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 
 type Props = {
     link: string,
-    name: string
+    name: string,
+    onClick?: () => void
 }
 
-const SidebarLinkButton: React.FC<Props> = ({ link, name }) => {
+const SidebarLinkButton: React.FC<Props> = ({ link, name, onClick = () => {} }) => {
     return (
-        <Link to={link} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+        <Link to={link} onClick={onClick} className="flex items-center p-2 text-gray-900 rounded-lg text-white hover:bg-gray-100 hover:bg-gray-700 group">
             <span className="ms-3">{name}</span>
         </Link>
     )
